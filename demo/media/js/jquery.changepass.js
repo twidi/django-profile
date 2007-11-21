6e0932fd671eb1e6c29abfc3bdc3f6a6de054b8f
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2007-11-21 16:26:00 +0000

    Iexplorer fixes and more things.
    git-svn-id: https://django-profile.googlecode.com/svn/trunk@22 7b2eb738-463f-0410-8820-43b14e3dcc91

--- demo/media/js/jquery.changepass.js
+++ demo/media/js/jquery.changepass.js
@@ -10,4 +10,33 @@ $(function(){
   });  
 
 	$("#id_newpass1").focus();
+
+	// Check passwords function
+  function passwordFn() {
+		this.status = false;
+
+		this.check = function() {
+			var msgbox = $("#passmsg2");
+			var pass1 = $("#id_newpass1").val();
+			var pass2 = $("#id_newpass2").val();
+
+			if (pass1.length > 0 && pass2.length > 0) {
+				if (pass1 != pass2) {
+					msgbox.css("color", "red");
+					msgbox.text("The passwords are not equal.");
+				} else {
+					msgbox.css("color", "green");
+					msgbox.text("Ok.");	
+					this.status = true;
+				}
+			} else if (pass1.length > 0 || pass2.length > 0) {
+				msgbox.text("");
+			}
+		}
+  }
+
+	var pass = new passwordFn();
+	$("#id_newpass1").blur(pass.check);
+	$("#id_newpass2").blur(pass.check);
+
 });
