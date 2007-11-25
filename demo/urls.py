1fb09bedbc412954e630fff8fcdd838adb2a538b
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2007-11-25 21:51:01 +0000

    Rework...
    git-svn-id: https://django-profile.googlecode.com/svn/trunk@31 7b2eb738-463f-0410-8820-43b14e3dcc91

--- demo/urls.py
+++ demo/urls.py
@@ -1,8 +1,9 @@
 from django.conf.urls.defaults import *
-from django.views.generic.simple import redirect_to, direct_to_template
-from profile.models import Profile
 
 urlpatterns = patterns('',
+
+    # Demo FrontPage
+    (r'^$', "profile.views.profiles", {'template': 'front.html' }),
 
     # Profile
     (r'^profile/', include('profile.urls')),
@@ -10,14 +11,11 @@ urlpatterns = patterns('',
     # Account
     (r'^accounts/', include('account.urls')),
 
-    # Demo FrontPage
-    (r'^$', "profile.views.profiles", {'template': 'front.html' }),
-
     # Serves media content. WARNING!! Only for development uses
     (r'^site_media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': 'media/'}),
 
 
-    # Admin
+    # Admin (not really needed)
     (r'^admin/', include('django.contrib.admin.urls')),
 
 )
