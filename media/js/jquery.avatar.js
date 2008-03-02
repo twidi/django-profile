a751a72282b393d63458f846621ba4915cb04ab7
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2008-03-02 09:07:21 +0000

    New crop&resize method
    git-svn-id: https://django-profile.googlecode.com/svn/trunk@65 7b2eb738-463f-0410-8820-43b14e3dcc91

--- media/js/jquery.avatar.js
+++ media/js/jquery.avatar.js
@@ -1,25 +1,25 @@
 $(function() {
 
-  $("input[@type=submit].buttoncancel").click(function() {
-		if ($("#avatar_id").val()) {
-    	$.post("/profile/avatar/delete/" + $("#avatar_id").val() + "/", function(data) {
-				window.close();
-			});
-		} else {
-			window.close();
-		}
-		return false;
+  $("input[@type=button].cancel").click(function() {
+		window.close();
   });
 
-	$("#id_photo").change(function() {
-		$("form").submit();
-		$("#loading").show();
+	$("a.cancel").click(function() {
+   	$.post($(this).attr("href"), function(data) {
+			window.close();
+		});
+		return false;
 	});
 
-  $("input[@type=submit].buttonanother").click(function() {
-    $.post("/profile/avatar/delete/" + $("#avatar_id").val() + "/", function(data) {
-			window.location = "/avatar/one/";
-			window.resizeTo(500, 220);
+	$("#id_photo").change(function() {
+		$("div.loading").show();
+		$("form").submit();
+	});
+
+  $("a.another").click(function() {
+    $.post($(this).attr("href"), function(data) {
+			window.resizeTo(410, 160);
+			window.location = "/profile/avatar/choose/";
     });
 		return false;
   });
@@ -30,24 +30,4 @@ $(function() {
 		$("#id_left").val(a.left);
 		$("#id_size").val($("#child").height());
   });
-
-  function bgmove() {
-    var pos = $("#child").position();
-    $("#child").css("background-position", "-" + pos.left + "px -" + pos.top + "px");
-  }
-
-	function imgclick(e) {
-		offset = $("#avatarzone").position();
-		$("#child").css( { left: e.clientX - offset.left-16, top: e.clientY - offset.top-16, width: '32px', height: '32px' });
-		$("#child").css( "background-position", "-" + (e.clientX - offset.left-16) + "px -" + (e.clientY - offset.top-16) + "px" );
-  }
-
-	$("#avatarzone").mousedown(imgclick);
-	$("#child").click(function(e) {
-		e.stopPropagation();
-		e.preventDefault();
-	});
-
-  $("#child").draggable({ 'containment': '#avatarzone', 'drag': bgmove }).resizable( { 'containment': '#avatarzone', 'minWidth': 32, 'minHeight': 32, 'resize': bgmove });
-
 });
