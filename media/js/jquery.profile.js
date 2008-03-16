356e0ed2dece0e6d273d0f5e76cb96f9d49d815c
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2008-03-16 22:03:13 +0000

    bug solved
    git-svn-id: https://django-profile.googlecode.com/svn/trunk@91 7b2eb738-463f-0410-8820-43b14e3dcc91

--- media/js/jquery.profile.js
+++ media/js/jquery.profile.js
@@ -81,7 +81,7 @@ $(function(){
   });  
 
 	var date = $("#id_birthdate").val().split("-");
-  $("div.calendar").datepicker({ onSelect: updateInline, hideIfNoPrevNext: true, yearRange: "1940:2010", defaultDate: "01/02/2005", defaultDate: new Date(date[0],date[1] -1, date[2]) });
+  $("div.calendar").datepicker({ onSelect: updateInline, hideIfNoPrevNext: true, yearRange: "1940:2010", defaultDate: new Date(date[0],date[1] -1, date[2]) });
   $("a.newavatar").click(function() {
 		window.open($(this).attr("href"), "Avatar", "width=410, height=160,resizable=yes").moveTo(100,100);
 		return false;
