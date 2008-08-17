82669619e9e3a467118e634abd0f285a77d5612b
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2008-08-17 07:40:39 +0000

    Changed the country attribute. Now the country can be blank. Changed the WEBSEARCH setting to AVATAR_WEBSEARCH.
    git-svn-id: https://django-profile.googlecode.com/svn/trunk@324 7b2eb738-463f-0410-8820-43b14e3dcc91

--- demo/settings.py
+++ demo/settings.py
@@ -95,7 +95,7 @@ INSTALLED_APPS = (
 
 # START of django-profile specific options
 DEFAULT_AVATAR = os.path.join(MEDIA_ROOT, 'avatars', 'generic.jpg')
-WEBSEARCH = True
+AVATAR_WEBSEARCH = True
 # 127.0.0.1:8000 Google Maps API Key
 GOOGLE_MAPS_API_KEY = "ABQIAAAA06IJoYHDPFMx4u3hTtaghxTpH3CbXHjuCVmaTc5MkkU4wO1RRhST5bKY_U7dUG1ZGu1S-n-ukXGNjQ"
 # END of django-profile specific options
