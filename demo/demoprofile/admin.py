efa97f1991157cd23d93bcba6591cc41b75ee16c
david.rubert <david.rubert@7b2eb738-463f-0410-8820-43b14e3dcc91>
2008-12-08 15:14:52 +0000

    Integrated avatar page with user messages framework, and solved a bug with the creation_date field.
    git-svn-id: https://django-profile.googlecode.com/svn/trunk@379 7b2eb738-463f-0410-8820-43b14e3dcc91

--- demo/demoprofile/admin.py
+++ demo/demoprofile/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from demoprofile.models import Profile
 
 class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('user', 'date')
+    list_display = ('user', 'creation_date')
     search_fields = ('name',)
 
 admin.site.register(Profile, ProfileAdmin)
