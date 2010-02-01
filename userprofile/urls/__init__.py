c95a463171720ce2d2315818ea72353e33f70905
stephane.angel <stephane.angel@7b2eb738-463f-0410-8820-43b14e3dcc91>
2010-02-01 23:03:30 +0000

    add french urls, and update urls/__init__.py so it can handle urls for fr-fr via fr.py if fr-fr is not defined (idem for en-us : it will use en.py if en-us.py is not defined)
    git-svn-id: https://django-profile.googlecode.com/svn/trunk@425 7b2eb738-463f-0410-8820-43b14e3dcc91

--- userprofile/urls/__init__.py
+++ userprofile/urls/__init__.py
@@ -1,13 +1,19 @@
 from django.conf import settings
 
-if hasattr(settings, "I18N_URLS") and settings.I18N_URLS:
+try:
+    if not hasattr(settings, "I18N_URLS") or not settings.I18N_URLS \
+                                          or not settings.LANGUAGE_CODE:
+        raise
+
     try:
         module = __import__("userprofile.urls.%s" % (settings.LANGUAGE_CODE), \
                             {}, {}, "urlpatterns")
-        globals().update({ "urlpatterns": module.__dict__["urlpatterns"] })
     except:
-        module = __import__("userprofile.urls.en", {}, {}, "urlpatterns")
-        globals().update({ "urlpatterns": module.__dict__["urlpatterns"] })
-else:
+        module = __import__( \
+            "userprofile.urls.%s" % (settings.LANGUAGE_CODE.split('-')[0]), \
+            {}, {}, "urlpatterns")
+
+except:
     module = __import__("userprofile.urls.en", {}, {}, "urlpatterns")
-    globals().update({ "urlpatterns": module.__dict__["urlpatterns"] })
+
+globals().update({ "urlpatterns": module.__dict__["urlpatterns"] })
