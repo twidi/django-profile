75db4dabbd7f934d2c89412abde15466680221a2
stephane.angel <stephane.angel@7b2eb738-463f-0410-8820-43b14e3dcc91>
2010-02-02 18:00:14 +0000

    location could be blanck AND null (useful when installing django-profile with existing profiles)
    git-svn-id: https://django-profile.googlecode.com/svn/trunk@426 7b2eb738-463f-0410-8820-43b14e3dcc91

--- userprofile/models.py
+++ userprofile/models.py
@@ -40,7 +40,7 @@ class BaseProfile(models.Model):
     country = CountryField(null=True, blank=True)
     latitude = models.DecimalField(max_digits=10, decimal_places=6, blank=True, null=True)
     longitude = models.DecimalField(max_digits=10, decimal_places=6, blank=True, null=True)
-    location = models.CharField(max_length=255, blank=True)
+    location = models.CharField(max_length=255, blank=True, null=True)
 
     class Meta:
         abstract = True
